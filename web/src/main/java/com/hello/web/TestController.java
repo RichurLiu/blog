ac08737b8cf39eb7eb3a3f4de2ad39e62512d020
liulei <liulei06990@hellobike.com>
2019-02-02 13:29:01 +0800

    a

--- web/src/main/java/com/hello/web/TestController.java
+++ web/src/main/java/com/hello/web/TestController.java
@@ -1,17 +1,21 @@
 package com.hello.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * @Author: liu lei
  * @Date: 2019/1/31
  */
-@RestController
+@Controller
 public class TestController {
-
     @GetMapping("/test")
     public String test(){
-        return "hello bike";
+        System.out.println("hellllllooooooooooooo");
+        return "bike";
     }
 }
--- web/src/main/java/com/hello/web/book/BookController.java
+++ web/src/main/java/com/hello/web/book/BookController.java
@@ -1,9 +1,14 @@
 package com.hello.web.book;
 
+import com.alibaba.fastjson.JSON;
 import com.hello.biz.service.BookInfoService;
+import com.hello.model.dto.BookInfoRespDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author: liu lei
@@ -16,7 +21,9 @@ public class BookController {
     BookInfoService bookInfoService;
 
     @GetMapping("/book")
-    public String getAllBook(){
-        return bookInfoService.getAllBook().toString();
+    public List<BookInfoRespDTO> getAllBook(){
+//        return bookInfoService.getAllBook().toString();
+//        return JSON.toJSON(bookInfoService.getAllBook()).toString();
+        return bookInfoService.getAllBook();
     }
 }
